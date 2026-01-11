01f2f2de72032c2bb78477bb085bbc3405f21fe6
EdwinRamos6561 <eramosb08@gmail.com>
2026-01-10 21:20:13 -0500

    Proyecto inicial con Node y React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Index de la plataforma</h1>
+      <p>React funcionando correctamente</p>
+    </div>
+  );
+}
+
+export default App;
